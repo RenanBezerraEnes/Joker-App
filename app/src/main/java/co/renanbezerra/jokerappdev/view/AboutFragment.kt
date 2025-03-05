5184ffc0ad0c125a5ca4997e360083083e7e886a
Renan <renanbezerraenes@icloud.com>
2025-03-05 12:50:55 +0100

    add about view info

--- app/src/main/java/co/renanbezerra/jokerappdev/view/AboutFragment.kt
+++ app/src/main/java/co/renanbezerra/jokerappdev/view/AboutFragment.kt
@@ -1,10 +1,13 @@
-package co.renanbezerra.jokerappdev
+package co.renanbezerra.jokerappdev.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import co.renanbezerra.jokerappdev.BuildConfig
+import co.renanbezerra.jokerappdev.R
 
 class AboutFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +16,11 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.txt_version).text = getString(R.string.build_version, BuildConfig.VERSION_NAME)
     }
 }
